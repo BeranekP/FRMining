1c7c5f0b20ccf83ec01b6e238a995ca0f298b590
BeranekP <2422141+BeranekP@users.noreply.github.com>
2025-02-04 17:05:23 +0100

    add more info to geoJSON

--- process.py
+++ process.py
@@ -45,15 +45,24 @@ if len(sys.argv) < 2:
 flight_id = sys.argv[1]
 timestamp=""
 if len(sys.argv) == 3:
-    time = sys.argv[2]
-    dt = datetime.strptime(time, "%d/%m/%Y-%H:%M:%S")
-    timestamp = dt.timestamp()
+    scheduled_departure = sys.argv[2]
+    dt = datetime.strptime(scheduled_departure, "%d/%m/%Y-%H:%M")
+    timestamp = int(dt.timestamp())
     print(timestamp)
 
 data = get_data(flight_id, timestamp)
+max_heigth = 0
+max_speed = 0
+
 
 # get track data
 track = data["result"]["response"]["data"]["flight"]["track"]
+output["features"][0]["properties"]["flightId"] = flight_id
+output["features"][0]["properties"]["timestamp"] = timestamp
+output["features"][0]["properties"]["status"] = data["result"]["response"]["data"]["flight"]["status"]["generic"]
+output["features"][0]["properties"]["aircraft"] = data["result"]["response"]["data"]["flight"]["aircraft"]["model"]["text"]
+output["features"][0]["properties"]["airportOrigin"] = data["result"]["response"]["data"]["flight"]["airport"]["origin"]["name"]
+output["features"][0]["properties"]["airportDestination"] = data["result"]["response"]["data"]["flight"]["airport"]["destination"]["name"]
 
 
 # get origin and destination IATA codes
@@ -62,9 +71,20 @@ destination = data["result"]["response"]["data"]["flight"]["airport"]["destinati
 
 # put coordinate pairs in output dict
 for obj in track:
+    speed = obj["speed"]["kmh"]
+
     output["features"][0]["geometry"]["coordinates"].append(
-        [obj["longitude"], obj["latitude"]]
+        [obj["longitude"], obj["latitude"], obj["altitude"]["meters"]]
     )
+    if obj["altitude"]["meters"] > max_heigth:
+        max_heigth = obj["altitude"]["meters"]
+    if speed > max_speed:
+        max_speed = speed
+
+number = data["result"]["response"]["data"]["flight"]["identification"]["number"]["default"]
+output["features"][0]["properties"]["maxSpeed"] = max_speed
+output["features"][0]["properties"]["maxHeight"] = max_heigth
+output["features"][0]["properties"]["name"] = f'{number}: {origin} -> {destination}'
 
 # make json
 json_object = json.dumps(output, indent=4)
@@ -72,3 +92,6 @@ json_object = json.dumps(output, indent=4)
 # write to file
 with open(f"track {origin}-{destination}.json", "w") as outfile:
     outfile.write(json_object)
+
+
+
