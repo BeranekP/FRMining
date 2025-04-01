4db1d6327de66c8b7a5dbc7a20adc5f7e2d166b8
BeranekP <2422141+BeranekP@users.noreply.github.com>
2025-04-01 22:20:38 +0200

    minor update

--- process.py
+++ process.py
@@ -1,8 +1,8 @@
 import json
-from operator import ge
 import requests
 import sys
 from datetime import datetime
+from datetime import timezone
 
 
 
@@ -68,14 +68,24 @@ geoJSON["features"][0]["properties"]["airportDestination"] = data["result"]["res
 
 # get origin and destination IATA codes
 origin = data["result"]["response"]["data"]["flight"]["airport"]["origin"]["code"]["iata"]
+origin_name = data["result"]["response"]["data"]["flight"]["airport"]["origin"]["name"]
+origin_position = data["result"]["response"]["data"]["flight"]["airport"]["origin"]["position"]
+
 destination = data["result"]["response"]["data"]["flight"]["airport"]["destination"]["code"]["iata"]
+destination_name = data["result"]["response"]["data"]["flight"]["airport"]["destination"]["name"]
+destination_position = data["result"]["response"]["data"]["flight"]["airport"]["destination"]["position"]
+
+aircraft_reg=data["result"]["response"]["data"]["flight"]["aircraft"]["identification"]["registration"]
+planespotters = f'https://www.planespotters.net/search?q={aircraft_reg}'
+planespotters = "[[" + planespotters + "|aircraft detail]]"
 
 # put coordinate pairs in output dict
 for obj in track:
     speed = obj["speed"]["kmh"]
 
     geoJSON["features"][0]["geometry"]["coordinates"].append(
-        [obj["longitude"], obj["latitude"], obj["altitude"]["meters"]]
+        [obj["longitude"], obj["latitude"], obj["altitude"]["meters"], obj["speed"]["kmh"]
+]
     )
     if obj["altitude"]["meters"] > max_heigth:
         max_heigth = obj["altitude"]["meters"]
@@ -91,20 +101,21 @@ geoJSON["features"][0]["properties"]["name"] = f'{number}: {origin} -> {destinat
 json_object = json.dumps(geoJSON, indent=4)
 
 # write to file
-with open(f"track_{origin}-{destination}{datetime.now().strftime('%Y%M%d_%H%M%S')}.json", "w") as outfile:
+with open(f"track_{origin}-{destination}-{datetime.now().strftime('%Y%M%d_%H%M%S')}.json", "w") as outfile:
     outfile.write(json_object)
 
 
-with open( f"track_{origin}-{destination}{datetime.now().strftime('%Y%M%d_%H%M%S')}.gpx", "w") as gpxfile:
+with open( f"track_{origin}-{destination}-{datetime.now().strftime('%Y%M%d_%H%M%S')}.gpx", "w", encoding="utf-8") as gpxfile:
     gpxfile.write('<?xml version="1.0" encoding="UTF-8"?>\n')
     gpxfile.write('''<gpx version="1.0"
      creator="Totot Jatotrava"
      xmlns="http://www.topografix.com/GPX/1/0"
      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
      xsi:schemaLocation="http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd">\n''')
-    gpxfile.write(f'''<desc>Aircraft: {data["result"]["response"]["data"]["flight"]["aircraft"]["model"]["text"]}; max_speed: {max_speed}</desc>\n''')
     gpxfile.write("<keywords>flight</keywords>\n")
-    gpxfile.write(f"<trk><name>{number}: {origin} -> {destination}'</name>\n<trkseg>\n")
+    gpxfile.write(f"<trk><name>{number}: {origin} -> {destination}</name>\n")
+    gpxfile.write(f'''<desc>**Aircraft**: {data["result"]["response"]["data"]["flight"]["aircraft"]["model"]["text"]}({aircraft_reg})\n **Max speed**: {max_speed}\n **Flight id**: {flight_id}\n{planespotters}</desc>\n''')
+    gpxfile.write("<trkseg>\n")
     for obj in track:
            speed = obj["speed"]["kmh"]/3.6
            lat = obj["latitude"]
@@ -112,10 +123,15 @@ with open( f"track_{origin}-{destination}{datetime.now().strftime('%Y%M%d_%H%M%S
            alt = obj["altitude"]["meters"]
            course = obj["heading"]
            t = obj["timestamp"]
-           time = datetime.utcfromtimestamp(t).isoformat()
+           time = datetime.fromtimestamp(float(t), tz=timezone.utc).isoformat()
            gpxfile.write(f'<trkpt lat="{lat}" lon="{lon}"><ele>{alt}</ele><speed>{speed}</speed><course>{course}</course><time>{time}</time></trkpt>\n')
 
 
-    gpxfile.write("</trkseg>\n</trk>\n</gpx>\n")    
+    gpxfile.write("</trkseg>\n</trk>\n")
+    
+    gpxfile.write(f'<wpt lat="{origin_position["latitude"]}" lon="{origin_position["longitude"]}"><name>{origin_name}</name><sym>✈</sym><type>Origin</type></wpt>\n')
+    gpxfile.write(f'<wpt lat="{destination_position["latitude"]}" lon="{destination_position["longitude"]}"><name>{destination_name}</name><sym>✈️</sym><type>Destination</type></wpt>\n')
+
+    gpxfile.write("</gpx>")
 
 
